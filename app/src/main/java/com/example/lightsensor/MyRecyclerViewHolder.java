09e20334a086f2427a634388e2f363cb0ec0ebb6
abbas443 <nadakkaa@tcd.ie>
2019-03-15 10:50:14 +0000

    Added google

---
+++ app/src/main/java/com/example/lightsensor/MyRecyclerViewHolder.java
@@ -0,0 +1,18 @@
+package com.example.lightsensor;
+
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class MyRecyclerViewHolder extends RecyclerView.ViewHolder {
+
+
+    public MyRecyclerViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+
+    }
+}
+
