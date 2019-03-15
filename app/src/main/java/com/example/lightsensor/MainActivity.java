c0d28dc92cdbf07b52598d721b515f9663dceea1
abbas443 <nadakkaa@tcd.ie>
2019-03-15 10:48:39 +0000

    Added google

--- app/src/main/java/com/example/lightsensor/MainActivity.java
+++ app/src/main/java/com/example/lightsensor/MainActivity.java
@@ -5,13 +5,52 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.UnsupportedMimeTypeException;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.IOException;
+
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
+    public static void getCalories() {
+        try {
+            Document document = Jsoup.connect("https://www.google.com/search?q=dublin+temperature").get();
+
+            Element element =  document.select("div[aria-live]").first();
+            System.out.println(element.text());
+            Log.d(TAG, "Time Stamp: "  + " Light: " );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        MainActivity.getCalories();
+
+
+    }
     private static final String TAG = "Sensor";
 
 
@@ -19,17 +58,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor mLight;
     TextView light;
 
+
+    RecyclerView recyclerView;
+
+    //Firebase
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    FirebaseRecyclerOptions<Post> options;
+    FirebaseRecyclerAdapter<Post,MyRecyclerViewHolder> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        light = (TextView) findViewById(R.id.light);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("EDMT_FIREBASE");
 
+        light = (TextView) findViewById(R.id.light);
 
         Log.d(TAG, "onCreate: Initializing Sensor Services");
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-
         mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         if (mLight != null) {
             sensorManager.registerListener((SensorEventListener) MainActivity.this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
@@ -38,13 +88,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             light.setText("Light meter Not Supported");
         }
 
-
+        displayComment();
 
     }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
+    private void displayComment() {
+        options =
+                new FirebaseRecyclerOptions.Builder<Post>()
+                .setQuery(databaseReference,Post.class)
+                .build();
+        adapter=
+                new FirebaseRecyclerAdapter<Post, MyRecyclerViewHolder>(options) {
+                    @Override
+                    protected void onBindViewHolder(@NonNull MyRecyclerViewHolder holder, int position, @NonNull Post model) {
+
+                   }
+
+                    @NonNull
+                    @Override
+                    public MyRecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+                        View itemView = LayoutInflater.from(getBaseContext()).inflate(R.layout.post_item,viewGroup,false);
+                        return new MyRecyclerViewHolder(itemView);
+                    }
+                };
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
     }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
 
@@ -58,12 +129,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             String tss = tssLong.toString();
 
             light.setText(" "+sensorEvent.values[0]);
-            Log.d(TAG, "Time Stamp: " + ts + " Light: " + sensorEvent.values[0]);
+            String Light = Float.toString(sensorEvent.values[0]);
+            //Log.d(TAG, "Time Stamp: " + ts + " Light: " + sensorEvent.values[0]);
+            Post post = new Post(ts,Light);
+
+            databaseReference.push()
+                    .setValue(post);
+            adapter.notifyDataSetChanged();
 
 
         }
 
+
     }
 
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
 }
